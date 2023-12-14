614712923088c3ebe4d18c522984f30a018516b6
BT_1N3_16 <nelsons64@nycstudents.net>
2023-12-14 15:35:38 -0500

    Nelly

---
+++ Panel.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.*;
+public class Panel extends JPanel
+{
+    final int PANEL_WIDTH = 500;
+    final int PANEL_HEIGHT = 500;
+    Panel()
+    {
+        this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+    }
+}
